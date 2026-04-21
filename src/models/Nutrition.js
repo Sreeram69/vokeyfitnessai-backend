86d59e18af30455da34980f82ff1c8152acca0bc
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-04-21 16:58:24 +0530

    added initial Nutrition model skeleton

---
+++ src/models/Nutrition.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const nutritionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    meals: [
