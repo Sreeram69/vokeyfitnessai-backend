c47a0cebb5790c3e38d96cb7714f2291a4938e9c
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-04-23 17:11:29 +0530

    added initial User model skeleton

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
