0110ede099825f3f309111f527a4c80f2259844e
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-04-24 13:21:00 +0530

    created Workout mongoose schema and model

--- src/models/Workout.js
+++ src/models/Workout.js
@@ -13,3 +13,30 @@ const workoutSchema = new mongoose.Schema(
       type: String,
       enum: ["Beginner", "Intermediate", "Advanced"],
       default: "Beginner",
+    },
+    duration: {
+      type: Number, // recommended duration in minutes
+      default: 45,
+    },
+    exercises: [
+      {
+        name: String,
+        sets: String,
+        reps: String,
+      }
+    ],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+workoutSchema.index({ title: 1 });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
