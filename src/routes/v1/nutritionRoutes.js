0b276632254547cb4c9d1dce9d2bf2f4b0003c78
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-05-11 11:24:48 +0530

    added express routes for nutrition endpoints

--- src/routes/v1/nutritionRoutes.js
+++ src/routes/v1/nutritionRoutes.js
@@ -13,3 +13,27 @@ const {
   getAiSuggestions,
   getAllFoods,
   createFood,
+  updateFood,
+  deleteFood
+} = require("../../controllers/nutritionController");
+const { protect, adminOnly } = require("../../middleware/authMiddleware");
+const validate = require("../../middleware/validate");
+const { logMealSchema, getNutritionSchema } = require("../../validators/nutritionValidator");
+
+router.route("/search").get(protect, searchFood);
+router.route("/barcode/:upc").get(protect, lookupBarcode);
+router.route("/suggestions").get(protect, getAiSuggestions);
+
+router.route("/foods")
+  .get(protect, getAllFoods)
+  .post(protect, adminOnly, createFood);
+
+router.route("/foods/:name")
+  .put(protect, adminOnly, updateFood)
+  .delete(protect, adminOnly, deleteFood);
+
+router.route("/")
+  .post(protect, validate({ body: logMealSchema }), logMeal)
+  .get(protect, validate({ query: getNutritionSchema }), getNutrition);
+
+module.exports = router;
